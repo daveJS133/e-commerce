b5a623d3fc9809a4ee938ab5bfab18e813e48ec8
Dave <dave@Daves-MBP.lan>
2017-07-27 11:23:29 +0100

    Fixes dependency conflicts and changes file paths for updated versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -170,7 +170,7 @@ module.exports = function (grunt) {
       options: {
         map: true,
         processors: [
-          require('autoprefixer')({browsers: ['last 1 version']})
+          require('autoprefixer')({browsers: ['last 2 versions']})
         ]
       },
       dist: {
