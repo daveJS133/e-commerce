cd4f001509a7c3f6e830326a95420898a955b99f
Dave <dave@Daves-MBP.lan>
2017-08-06 13:16:41 +0100

    adds product component

--- client/app/app.js
+++ client/app/app.js
@@ -22,6 +22,7 @@ import admin from './admin';
 import navbar from '../components/navbar/navbar.component';
 import footer from '../components/footer/footer.component';
 import main from './main/main.component';
+import products from './products/products.component';
 import constants from './app.constants';
 import util from '../components/util/util.module';
 import socket from '../components/socket/socket.service';
